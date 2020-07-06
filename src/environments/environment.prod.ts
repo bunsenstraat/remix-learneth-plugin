2f5806c202a2daca36bb6996263da6f00d365c6c
filip mertens <filipmertens@filips-iMac.local>
2020-07-06 21:28:13 +0200

    icon bigger rm stroumpf from env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   base: "./",
   apiUrl: `https://static.220.14.12.49.clients.your-server.de:3000/`,
-  github:[{name:`ethereum/remix-workshops`,branch:`master`},{name:`GrandSchtroumpf/solidity-school`,branch:`master`}],
+  github:[{name:`ethereum/remix-workshops`,branch:`master`}],
   help: 'https://github.com/bunsenstraat/remix-workshop-plugin/blob/master/README.md'
 };
