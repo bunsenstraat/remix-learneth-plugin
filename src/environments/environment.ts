38afc36e060ac716ac29b8338f8487925e65867a
filip mertens <filipmertens@filips-iMac.local>
2020-09-27 12:27:07 +0200

    env api url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: false,
   base: "/",
-  //apiUrl: `https://static.220.14.12.49.clients.your-server.de:3000/`,
-  apiUrl: `http://localhost:3001/`,
+  apiUrl: `https://static.220.14.12.49.clients.your-server.de:3000/`,
+  //apiUrl: `http://localhost:3001/`,
   github:[{name:`ethereum/remix-workshops`,branch:`master`},{name:`GrandSchtroumpf/solidity-school`,branch:`master`}],
   help: 'https://github.com/bunsenstraat/remix-workshop-plugin/blob/master/README.md'
 };
